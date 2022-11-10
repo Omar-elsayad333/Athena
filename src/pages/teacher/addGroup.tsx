677976ee3fe719efe865f90b34258f5ab79ebcd9
dienoone <noonecazanofa@gmail.com>
2022-11-10 02:23:15 +0200

    from desktop

--- src/pages/teacher/addGroup.tsx
+++ src/pages/teacher/addGroup.tsx
@@ -1,14 +1,14 @@
-import TeacherLayout from "../../components/layout/teacherLayout/TeacherLayout";
+import TeacherLayout from "../../components/Layout/teacherLayout/TeacherLayout";
 
 // MUI
 import Box from "@mui/material/Box";
 
 const AddGroup = () => {
-    return (
-        <Box>
-            <TeacherLayout />
-        </Box>
-    );
-}
+  return (
+    <Box>
+      <TeacherLayout />
+    </Box>
+  );
+};
 
 export default AddGroup;
