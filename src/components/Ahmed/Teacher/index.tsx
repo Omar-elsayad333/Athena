11b05a89726252a654462030b979a7d49a20fea7
omar-elsayad <kklskull1234@gmail.com>
2022-11-11 06:43:24 +0200

    from labtop

--- src/components/Ahmed/Teacher/index.tsx
+++ src/components/Ahmed/Teacher/index.tsx
@@ -1,21 +1,20 @@
 import style from './style';
 import Image from 'next/image';
-// import NavLink from './NavLink';
+import NavLink from './NavLink';
 import logo from '../../../../public/images/secondaryInlinelogo.svg';
-// import logo from "../../../../public/images/secondaryInlineLogo.svg";
-// import menu from '../../../../public/images/Menu icon.svg';
-// import logOut from '../../../../public/images/LogOut icon.svg';
-// import home from '../../../../public/images/Home icon.svg';
-// import students from '../../../../public/images/Students icon.svg';
-// import groups from '../../../../public/images/Groups icon.svg';
-// import calender from '../../../../public/images/Calender icon.svg';
-// import exams from '../../../../public/images/Exams icon.svg';
-// import location from '../../../../public/images/Location icon.svg';
-// import charts from '../../../../public/images/Charts icon.svg';
-// import year from '../../../../public/images/Year Icon.svg';
-// import money from '../../../../public/images/Money icon.svg';
-// import employees from '../../../../public/images/Employees icon.svg';
-// import setting from '../../../../public/images/Setting icon.svg';
+import menu from '../../../../public/images/Menu icon.svg';
+import logOut from '../../../../public/images/LogOut icon.svg';
+import home from '../../../../public/images/Home icon.svg';
+import students from '../../../../public/images/Students icon.svg';
+import groups from '../../../../public/images/Groups icon.svg';
+import calender from '../../../../public/images/Calender icon.svg';
+import exams from '../../../../public/images/Exams icon.svg';
+import location from '../../../../public/images/Location icon.svg';
+import charts from '../../../../public/images/Charts icon.svg';
+import year from '../../../../public/images/Year Icon.svg';
+import money from '../../../../public/images/Money icon.svg';
+import employees from '../../../../public/images/Employees icon.svg';
+import setting from '../../../../public/images/Setting icon.svg';
 
 // MUI
 import Box from '@mui/material/Box';
@@ -26,12 +25,12 @@ const Teacher: React.FC = () => {
         <Box sx={style.container}>
             <Box sx={style.header}>
                 <Box sx={style.header.menuIcon} >
-                    {/* <Image src={menu} layout='intrinsic' alt='menu icon' /> */}
+                    <Image src={menu} layout='intrinsic' alt='menu icon' />
                 </Box>
                 <Image src={logo} alt='Athena' width={228} height={43} />
             </Box>
             <Box sx={style.menu}>
-                {/* <Box>
+                <Box>
                     <NavLink icon={home} content='الرئيسية' />
                     <NavLink icon={students} content='الطلاب' />
                     <NavLink icon={groups} content='المجموعات' />
@@ -45,7 +44,7 @@ const Teacher: React.FC = () => {
                 <Box>
                     <NavLink icon={employees} content='الموظفيين' />
                     <NavLink icon={setting} content='الاعدادت' />
-                </Box> */}
+                </Box>
             </Box>
             <Box sx={style.footer}>
                 <Box sx={style.footer.privacy}>
@@ -59,7 +58,7 @@ const Teacher: React.FC = () => {
                         شروط الاستخدام وسياسة الخصوصية 
                     </Typography>
                 </Box>
-                {/* <Image src={logOut} alt='تسجيل الخروج' /> */}
+                <Image src={logOut} alt='تسجيل الخروج' />
             </Box>
         </Box>
     );
