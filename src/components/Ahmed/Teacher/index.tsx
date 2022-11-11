efb72780065d5c2c4ac6d687f6578b3a5a45a1b1
omar-elsayad <kklskull1234@gmail.com>
2022-11-11 06:45:03 +0200

    from labtop

--- src/components/Ahmed/Teacher/index.tsx
+++ src/components/Ahmed/Teacher/index.tsx
@@ -2,7 +2,7 @@ import style from './style';
 import Image from 'next/image';
 import NavLink from './NavLink';
 import logo from '../../../../public/images/secondaryInlinelogo.svg';
-import menu from '../../../../public/images/Menu icon.svg';
+import menu from '../../../../public/images/MenuIcon.svg';
 import logOut from '../../../../public/images/LogOut icon.svg';
 import home from '../../../../public/images/Home icon.svg';
 import students from '../../../../public/images/Students icon.svg';
