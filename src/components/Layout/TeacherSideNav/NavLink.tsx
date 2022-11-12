c5aca22229f1461c33c8b97d137c7b3e5731e7ab
omar-elsayad <kklskull1234@gmail.com>
2022-11-12 04:50:13 +0200

    from labtop

--- src/components/Layout/TeacherSideNav/NavLink.tsx
+++ src/components/Layout/TeacherSideNav/NavLink.tsx
@@ -45,7 +45,7 @@ const NavLink: React.FC<Prop> = ({icon, content, path, sideNavState}) => {
     return (
         <Link href={path}>
             <a>
-                <Button  className={ myStyle.myButton} onClick={(e) => select(e)}>
+                <Button  className={`${myStyle.myButton}`} onClick={(e) => select(e)}>
                     <Image src={icon} alt={content} layout='intrinsic' />
                     {sideNavState && content}
                 </Button>
