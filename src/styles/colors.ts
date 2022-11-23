a2016e09a3e6de93bfe91fb04c02a3dcfce7b3c6
dienoone <noonecazanofa@gmail.com>
2022-11-23 08:35:03 +0200

    from desktop

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,24 +1,43 @@
-const colors = {
+const lightColors = {
     primary: {
         main: '#3F72A4',
-        light: 'red',
-        dark: '#1C364F',
+        light: '#ffd700',
+        dark: '#B6D5F0',
         contrastText: '#E8F3FF',
     },
     secondary: {
-        main: '#B6D5F0',
-        light: 'red',
+        main: '#E8F3FF',
+        light: '#ffd700',
         dark: '#3F72A4',
-        contrastText: '#1C364F',
+        contrastText: '#3F72A4',
     },
-    white: '#8CC3F2',
-    button: {
-        primary: '#1C364F',
-        secondary: '#3F72A4',
+    backgroundColor: {
+        main: '#E0EEFF',
+        sideNav: '#B6D5F0',
+        navbar: '#B6D5F0',
+        navLogo: 'linear-gradient(90deg, #B6D5F0 0%, #DFEFFF 100%)',
     },
-    text: {
-        primary: '#E8F3FF',
-    }
 }
 
-export default colors
+const darkColors = {
+    primary: {
+        main: '#E0EEFF',
+        light: 'red',
+        dark: '#1C364F',
+        contrastText: '#1C364F',
+    },
+    secondary: {
+        main: 'transparent',
+        light: 'red',
+        dark: '#E0EEFF',
+        contrastText: '#E0EEFF',
+    },
+    backgroundColor: {
+        main: '',
+        sideNav: '#1C364F',
+        navbar: 'linear-gradient(0deg, #4072A4 0%, #1D3750 100%)',
+        navLogo: 'linear-gradient(0deg, #4072A4 0%, #1D3750 100%)',
+    },
+}
+
+export {lightColors, darkColors}
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,6 @@
+import { lightColors , darkColors } from './colors';
 import { useContext } from 'react';
 import { DarkThemeContext } from 'context/ThemeContext';
-import colors from './colors';
 
 // MUI
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -10,117 +10,159 @@ type IProps = {
 };
 
 
-//  them component
+// them component
 const ThemeApp: React.FC<IProps> = ({ children }) => {
 
   const {darkMode} = useContext(DarkThemeContext);
 
-  // Create a theme instance.x
-  const theme = createTheme({
+  // Light Theme
+  const lightTheme = createTheme({
     palette: {
       primary: {
-        main: colors.primary.main,
-        light: colors.primary.light,
-        dark: colors.primary.dark,
-        contrastText: colors.primary.contrastText,
+        main: lightColors.primary.main,
+        light: lightColors.primary.light,
+        dark: lightColors.primary.dark,
+        contrastText: lightColors.primary.contrastText,
       },
       secondary: {
-        main: colors.secondary.main,
-        light: colors.secondary.light,  
-        dark: colors.secondary.dark,
-        contrastText: colors.secondary.contrastText,    
+        main: lightColors.secondary.main,
+        light: lightColors.secondary.light,  
+        dark: lightColors.secondary.dark,
+        contrastText: lightColors.secondary.contrastText,    
       },
     },
     typography: {
-      fontFamily: 'Almarai, sans-serif'
-    },
-    components: {
+      fontFamily: 'Almarai, sans-serif',
+      h1: {
+        fontWeight: '700',
+        fontSize: '30px',
+        '@media (min-width:600px)': {
+          fontSize: '32px',
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '35px',
+        }
+      },
+      h2: {
+        fontWeight: '700',
+        fontSize: '25px',
+        '@media (min-width:600px)': {
+          fontSize: '27px',
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '30px',
+        }
+      },
+      h3: {
+        fontWeight: '700',
+        fontSize: '20px',
+        '@media (min-width:600px)': {
+          fontSize: '23px',        
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '25px',       
+        },
+      },
+      h4: {
+        fontWeight: '400',
+        fontSize: '18px',
+        '@media (min-width:1500px)': {
+          fontSize: '20px',       
+        },
+      },
+      h5: {
+        fontWeight: '400',
+        fontSize: '13px',
+        '@media (min-width:1500px)': {
+          fontSize: '14px',       
+        },
+      },
+      h6: {
+        fontWeight: '300',
+        fontSize: '10px',
+        '@media (min-width:1500px)': {
+          fontSize: '12px',       
+        },
+      },
     },
     direction: 'rtl',
   });
 
+  // Dark Theme
   const darkTheme = createTheme({
     palette: {
       primary: {
-        main: colors.secondary.main ,
-        light: colors.primary.light,
-        dark: colors.primary.dark,
-        contrastText: colors.primary.contrastText,
+        main: darkColors.primary.main,
+        light: darkColors.primary.light,
+        dark: darkColors.primary.dark,
+        contrastText: darkColors.primary.contrastText,
       },
       secondary: {
-        main: colors.secondary.main,
-        light: colors.secondary.light,  
-        dark: colors.secondary.dark,
-        contrastText: colors.secondary.contrastText,    
+        main: darkColors.secondary.main,
+        light: darkColors.secondary.light,  
+        dark: darkColors.secondary.dark,
+        contrastText: darkColors.secondary.contrastText,    
       },
     },
     typography: {
-      fontFamily: 'Almarai, sans-serif'
-    },
-    components: {
+      fontFamily: 'Almarai, sans-serif',
+      h1: {
+        fontWeight: '700',
+        fontSize: '30px',
+        '@media (min-width:600px)': {
+          fontSize: '32px',
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '35px',
+        }
+      },
+      h2: {
+        fontWeight: '700',
+        fontSize: '25px',
+        '@media (min-width:600px)': {
+          fontSize: '27px',
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '30px',
+        }
+      },
+      h3: {
+        fontWeight: '700',
+        fontSize: '20px',
+        '@media (min-width:600px)': {
+          fontSize: '23px',        
+        },
+        '@media (min-width:1500px)': {
+          fontSize: '25px',       
+        },
+      },
+      h4: {
+        fontWeight: '400',
+        fontSize: '18px',
+        '@media (min-width:1500px)': {
+          fontSize: '20px',       
+        },
+      },
+      h5: {
+        fontWeight: '400',
+        fontSize: '13px',
+        '@media (min-width:1500px)': {
+          fontSize: '14px',       
+        },
+      },
+      h6: {
+        fontWeight: '300',
+        fontSize: '10px',
+        '@media (min-width:1500px)': {
+          fontSize: '12px',       
+        },
+      },
     },
     direction: 'rtl',
   });
 
-  theme.typography.h1 = {
-    fontWeight: '700',
-    fontSize: '30px',
-    '@media (min-width:600px)': {
-      fontSize: '32px',
-    },
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '35px',
-    }
-  };
-
-  theme.typography.h2 = {
-    fontWeight: '700',
-    fontSize: '25px',
-    '@media (min-width:600px)': {
-      fontSize: '27px',
-    },
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '30px',
-    }
-  };
-
-  theme.typography.h3 = {
-    fontWeight: '700',
-    fontSize: '20px',
-    '@media (min-width:600px)': {
-      fontSize: '22px',
-    },
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '25px',
-    }
-  };
-
-  theme.typography.h4 = {
-    fontWeight: '400',
-    fontSize: '18px',
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '20px',
-    }
-  };
-
-  theme.typography.h5 = {
-    fontWeight: '400',
-    fontSize: '13px',
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '14px',
-    }
-  };
-
-  theme.typography.h6 = {
-    fontWeight: '300',
-    fontSize: '11px',
-    [theme.breakpoints.up('lg')]: {
-      fontSize: '12px',
-    }
-  };
-
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : theme} >
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme} >
       {children}
     </ThemeProvider>
   )
