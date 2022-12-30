aa5b821fbcb52a064691bb64734bc3a6f4fa8c4e
Omar-elsayad333 <omarelsayad333@gmail.com>
2022-12-30 18:19:15 +0200

    from desktop

--- src/components/Teacher/Students/FilterWedgit.tsx
+++ src/components/Teacher/Students/FilterWedgit.tsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
 import { useContext } from "react";
 import { DarkThemeContext } from "context/ThemeContext";
 
 // MUI
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 
 const FilterWedgit: React.FC = () => {
 
