939359b53b269e3db349fdb405d5fede35b13ae2
omar-elsayad <kklskull1234@gmail.com>
2023-01-07 02:23:20 +0200

    from labtop

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,48 +1,58 @@
 import CircularProgress from '@mui/material/CircularProgress';
 import { Box } from '@mui/system';
 import clsx from 'clsx';
+import { DarkThemeContext } from 'context/ThemeContext';
+import { useContext } from 'react';
 
 import { IStyle } from 'styles/IStyle';
 
-const styles: IStyle = {
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: '99',
-  },
-  inside: {
-    height: 'auto',
-    marginTop: '30px',
-    marginBottom: '20px',
-    zIndex: '999',
-  },
-  circle: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    margin: 'auto',
-    zIndex: '999',
-  },
-};
 
 type IProps = {
-    small?: boolean | undefined;
-    inside?: boolean | undefined;
+  small?: boolean | undefined;
+  inside?: boolean | undefined;
 };
 
 const Loading: React.FC<IProps> = ({ small, inside }) => {
-    console.log('loading');
+    const { mainColors } = useContext(DarkThemeContext);
+
+    const styles: IStyle = {
+      root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: '99',
+        background: 'red'
+      },
+      container: {
+        width: '100%',
+        height: '100vh',
+        background: mainColors.backgroundColor.main
+      },
+      inside: {
+        height: 'auto',
+        marginTop: '30px',
+        marginBottom: '20px',
+        zIndex: '999',
+      },
+      circle: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        margin: 'auto',
+        zIndex: '999',
+      },
+    };
+
     return (
         <>
             {inside ? (
-                <Box className={clsx(styles.root, styles.inside)}>
+                <Box className={clsx(styles.root, styles.inside, styles.container)}>
                     <CircularProgress size={small ? 30 : 75} />
                 </Box>
             ) : (
-                <Box>
+                <Box sx={styles.container}>
                     <CircularProgress size={small ? 30 : 75} sx={styles.circle} />
                 </Box>
             )}
