5642392669dc41fe2cf0f76f7d57f95935671dcb
omar-elsayad <kklskull1234@gmail.com>
2023-01-17 09:40:13 +0200

    from labtop

--- src/components/Teacher/Years/index.tsx
+++ src/components/Teacher/Years/index.tsx
@@ -1,25 +1,116 @@
-import YearCard from "./YearCard";
+import Link from "next/link";
+import { useContext } from "react";
+import useYears from "container/years/useYears";
+import { DarkThemeContext } from "context/ThemeContext";
 
 // MUI
 import Box from "@mui/material/Box";
-import useYears from "container/years/useYears";
+import Typography from "@mui/material/Typography";
+import Loading from "components/Loading/Loading";
 
 const YearsC: React.FC = () => {
 
-    const { data } = useYears()
+    const { 
+        data,
+        states
+    } = useYears();
+    const {mainColors} = useContext(DarkThemeContext);
 
     const style = {
         container: {
             display: 'flex',
+            alignItems: 'start',
+            flexWrap: 'wrap',
+            gap: '55px',
+        },
+        card: {
+            width: '335px',
+            maxWidth: '100%',
+            height: '149px',
+            padding: '40px 30px',
+            display: 'flex',
             flexDirection: 'column',
-            gap: '60px',
-        }
+            justifyContent: 'center',
+            alignItems: 'start',
+            gap: '35px',
+            border: '2px solid #3F72A4',
+            background: mainColors.linerGradient.primary,
+            borderRadius: '18px',
+            overflow: 'hidden',
+            cursor: 'pointer',
+            transition: '.35s',
+            ':hover': {
+                boxShadow: '0px 0px 15px 0px rgba(63, 114, 164, .50)',
+            },
+            '@media(max-width: 400px)': {
+                gap: '25px',
+                padding: '40px 20px',
+            },
+        },
+        content: {
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '20px',
+        },
+        spcialCard: {
+            width: '335px',
+            maxWidth: '100%',
+            height: '149px',
+            padding: '40px 30px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            border: '2px solid #3F72A4',
+            background: mainColors.dialog.background,
+            borderRadius: '18px',
+            overflow: 'hidden',
+            cursor: 'pointer',
+            transition: '.2s',
+            ':hover': {
+                boxShadow: '0px 0px 15px 0px rgba(63, 114, 164, .50)',
+            },
+            '@media(max-width: 400px)': {
+                gap: '25px',
+                padding: '40px 20px',
+            },
+        },
+        span: {
+            fontWeight: '700',
+        },
     }
 
     return (
         <Box sx={style.container}>
-            <YearCard data={data} />
-        </Box>
+            {
+                states.loading && <Loading />
+            }
+            {
+                data.yearsData &&
+                data.yearsData.map((item:any) => {
+                    return (
+                        <Link key={item.id} href={`/teacher/years/year/${item.id}`}>
+                            <Box sx={style.card}>
+                                <Box sx={style.content}>
+                                    <Typography color='primary' variant="h4" fontWeight={700}> 
+                                        العام الدراسي
+                                    </Typography>
+                                    <Typography color='primary' variant="h1">
+                                        {`${item.end} / ${item.start}`}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                        </Link>
+                    )
+                })
+            }
+            <Link href={`/teacher/years/add-year`}>
+                <Box sx={style.spcialCard}>
+                    <Typography color='primary' variant="h1" fontWeight={700}> 
+                        بدأ عام جديد
+                    </Typography>   
+                </Box>
+            </Link>
+        </Box>        
     );
 }
  
--- src/container/years/useYears.tsx
+++ src/container/years/useYears.tsx
@@ -1,140 +1,44 @@
+import { URL_YEARS } from 'constant/url'
+import { useState, useEffect } from 'react';
+import { useUser } from 'context/userContext';
+import { getHandler } from 'handlers/requestHandler';
+
 const useYears = () => {
 
-    const data:any = [
-        {
-            id: '1',
-            period: {
-                start: '2022 / 08 / 01',
-                end: '2023 / 06 / 01'
-            },
-            classes: [
-                {
-                    id: '1',
-                    name: 'أولى إعدادي'
-                },
-                {
-                    id: '2',
-                    name: 'ثانية إعدادي'
-                },
-                {
-                    id: '3',
-                    name: 'ثالثة إعدادي'
-                },
-                {
-                    id: '4',
-                    name: 'أولى ثانوي'
-                },
-                {
-                    id: '5',
-                    name: 'ثانية ثانوي'
-                },
-                {
-                    id: '6',
-                    name: 'ثالثة ثانوي'
-                }
-            ],
-            classroom: [
-                {
-                    id: '1',
-                    name: 'فصل دراسي أول'
-                },
-                {
-                    id: '2',
-                    name: 'فصل دراسي ثاني'
-                }
-            ]
-        },
-        {
-            id: '2',
-            period: {
-                start: '2021 / 08 / 01',
-                end: '2022 / 06 / 01'
-            },
-            classes: [
-                {
-                    id: '1',
-                    name: 'أولى إعدادي'
-                },
-                {
-                    id: '2',
-                    name: 'ثانية إعدادي'
-                },
-                {
-                    id: '3',
-                    name: 'ثالثة إعدادي'
-                },
-                {
-                    id: '4',
-                    name: 'أولى ثانوي'
-                },
-                {
-                    id: '5',
-                    name: 'ثانية ثانوي'
-                },
-                {
-                    id: '6',
-                    name: 'ثالثة ثانوي'
-                }
-            ],
-            classroom: [
-                {
-                    id: '1',
-                    name: 'فصل دراسي أول'
-                },
-                {
-                    id: '2',
-                    name: 'فصل دراسي ثاني'
-                }
-            ]
-        },
-        {
-            id: '3',
-            period: {
-                start: '2020 / 08 / 01',
-                end: '2021 / 06 / 01'
-            },
-            classes: [
-                {
-                    id: '1',
-                    name: 'أولى إعدادي'
-                },
-                {
-                    id: '2',
-                    name: 'ثانية إعدادي'
-                },
-                {
-                    id: '3',
-                    name: 'ثالثة إعدادي'
-                },
-                {
-                    id: '4',
-                    name: 'أولى ثانوي'
-                },
-                {
-                    id: '5',
-                    name: 'ثانية ثانوي'
-                },
-                {
-                    id: '6',
-                    name: 'ثالثة ثانوي'
-                }
-            ],
-            classroom: [
-                {
-                    id: '1',
-                    name: 'فصل دراسي أول'
-                },
-                {
-                    id: '2',
-                    name: 'فصل دراسي ثاني'
-                }
-            ]
-        },
-    ]
+    const auth = useUser()
+    const [ yearsData, setYearsData] = useState<any>();
+    const [ loading, setLoading] = useState<boolean>(false);
+
+    // Get the data of this page
+    useEffect(() => {
+        if(auth) {
+            getYearsData();
+        }
+    }, [auth])
+
+    // Get Years data from db
+    const getYearsData = async () => {
+        try {
+            setLoading(true);
+            const res = await getHandler(auth.authToken, URL_YEARS);
+            setYearsData(res);
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
 
     return (
         {
-            data
+            states: {
+                loading
+            },
+            data: {
+                yearsData
+            }
         }
     );
 }
