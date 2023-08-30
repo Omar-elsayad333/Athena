bcc51f9cf11f7b7c540207e30c5faeb4c560f76f
MRaghebdev <MohamedRagheb@geexar.dev>
2023-08-31 02:57:19 +0300

    add all ui and logic in page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from 'next'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Link from 'next/link'
+import Button from '@mui/material/Button'
 import { IStyle } from 'styles/IStyle'
 import { useTheme } from 'context/ThemeContext'
-import ErrorPageHeaderImg from 'components/BigImages/ErrorPageHeaderImg'
+import ErrorPageHeaderImg from 'components/BigImages/404Page/ErrorPageHeaderImg'
+import ErrorPageContentBoxImg from 'components/BigImages/404Page/ErrorPageContentBoxImg'
 import PageFooter from 'components/Shared/PageFooter'
 
 const ErrorPage: NextPage = () => {
@@ -10,10 +14,11 @@ const ErrorPage: NextPage = () => {
 
     const style: IStyle = {
         root: {
-            width: '100%',
+            width: '100vw',
             minHeight: '100vh',
             display: 'flex',
             flexDirection: 'column',
+            flexWrap: 'wrap',
             backgroundColor: mainColors.backgroundColor.main,
             transition: '.2s',
         },
@@ -23,16 +28,42 @@ const ErrorPage: NextPage = () => {
             position: 'absolute',
             display: 'flex',
             justifyContent: 'center',
+            FlexDirection: 'row',
             paddingTop: '20px',
             top: '0px',
         },
         container: {
-            backgroundColor: 'red',
             height: 'fit-content',
             display: 'flex',
-            position: 'relative',
-            top: '45vh',
+            position: 'absolute',
+            flexDirection: 'column',
+            top: '30vh',
+            right: '30vw',
             justifyContent: 'center',
+            alignContent: 'flex-end',
+        },
+        Text: {
+            color: mainColors.primary.main,
+            paddingRight: '11px',
+            alignSelf: 'end',
+            paddingBottom: '40px',
+        },
+        BigText: {
+            color: mainColors.primary.dark,
+            display: 'flex-inline',
+            fontWeight: '700',
+            fontSize: '120px',
+            alignSelf: 'end',
+            marginRight: '40px',
+        },
+        Text2: {
+            color: mainColors.primary.main,
+            fontSize: '40px',
+            fontWeight: '700',
+        },
+        Text3: {
+            marginTop: '10px',
+            fontWeight: '300',
         },
         footerContainer: {
             marginTop: 'auto',
@@ -45,7 +76,29 @@ const ErrorPage: NextPage = () => {
                 <Box sx={style.header}>
                     <ErrorPageHeaderImg />
                 </Box>
-                <Box sx={style.container}>testing</Box>
+                <Box sx={style.container}>
+                    <Box sx={{ display: 'inline-flex' }}>
+                        <ErrorPageContentBoxImg />
+                        <Typography sx={style.BigText}>404</Typography>
+                        <Typography variant="h5" sx={style.Text}>
+                            Error Code
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography sx={style.Text2}>عفوا! لم يتم العثور على هذه الصفحة</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h3" sx={style.Text3}>
+                            حدث خطأ ما في الصفحة التي تحاول زيارتها لم يتم العثور عليها
+                        </Typography>
+                    </Box>
+                    <Box sx={{ marginTop: '30px' }}>
+                        <Link href="/teacher/home">
+                            <Button variant="contained">العودة الى الرئيسية</Button>
+                        </Link>
+                    </Box>
+                </Box>
+
                 <PageFooter />
             </Box>
         </Box>
