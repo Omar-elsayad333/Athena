6940a413c623d7010bf645f9abdd1c8c6dd654a7
omar elsayad <omarelsayad313@gmail.com>
2024-05-24 16:15:10 +0300

    fix redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
+const { redirect } = require('next/dist/server/api-utils')
+
 module.exports = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'athenaapi.azurewebsites.net',
-                port: '',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'athenaapi.azurewebsites.net',
+        port: '',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/teacher-login',
+        permanent: true,
+      },
+    ]
+  },
 }
